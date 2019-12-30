c508e02f6d598dade6e71aefa3870ef417b17de6
mephet <matthew_kohjm@hotmail.sg>
2019-12-30 23:20:22 +0800

    github pages setup

---
+++ build/precache-manifest.5109ee400503eb3507a15ee9e908bae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3204ffb3045caea7dd43243b70f9829",
+    "url": "/mkjm/index.html"
+  },
+  {
+    "revision": "9087a7380ed067d96443",
+    "url": "/mkjm/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "d07e483690487aac66c5",
+    "url": "/mkjm/static/css/main.a03f50d8.chunk.css"
+  },
+  {
+    "revision": "9087a7380ed067d96443",
+    "url": "/mkjm/static/js/2.e5f9b286.chunk.js"
+  },
+  {
+    "revision": "b62c0e7f2e45ffccd681c408574ba18e",
+    "url": "/mkjm/static/js/2.e5f9b286.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d07e483690487aac66c5",
+    "url": "/mkjm/static/js/main.e25db6d1.chunk.js"
+  },
+  {
+    "revision": "c2f096cff2bbf5458a4d",
+    "url": "/mkjm/static/js/runtime-main.5cec4365.js"
+  }
+]);
